b32c80eb8b5934a45dcc856749a7fd9208b64c66
Shusnuke Oba <obashun22@yahoo.co.jp>
2023-04-27 16:36:19 +0900

    Update ファイル保存／タイムコード修正

--- Dockerfile
+++ Dockerfile
@@ -10,9 +10,10 @@ COPY requirements.txt requirements.txt
 # パッケージのインストール
 RUN pip install -r requirements.txt
 RUN pip install git+https://github.com/obashun22/whisper
+RUN apt update && apt install -y ffmpeg
 
 # requirements.txtを削除
 RUN rm requirements.txt
 
 # コンテナ起動時にflaskサーバを立ち上げるコマンドを実行するコマンド
-CMD ["gunicorn", "--bind", ":8080", "--workers", "3", "main:app"]
+CMD ["gunicorn", "--bind", ":8080", "--workers", "3", "--timeout", "300", "main:app"]
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import whisper
+import os
 from flask import Flask, request
 from flask_cors import CORS
 
 model = whisper.load_model("large")
+
+SAVE_DIR = "/uploads"
 
 
 def create_textfile(filename, sr, timelag):
@@ -17,11 +20,15 @@ def create_textfile(filename, sr, timelag):
             start_time += timelag
             end_time += timelag
             s_h, s_m = divmod(start_time, 3600)
-            s_m, s_s = divmod(s_m, 60)
+            s_m, s_sms = divmod(s_m, 60)
             e_h, e_m = divmod(end_time, 3600)
-            e_m, e_s = divmod(e_m, 60)
+            e_m, e_sms = divmod(e_m, 60)
+            s_h, e_h = int(s_h), int(e_h)  # 時
+            s_m, e_m = int(s_m), int(e_m)  # 分
+            s_s, e_s = int(s_sms), int(e_sms)  # 秒
+            s_ms, e_ms = int((s_sms - s_s) * 1000), int((e_sms - e_s) * 1000)  # ミリ秒
             f.write(
-                f'{index+1}\n{s_h:02}:{s_m:02}:{s_s:02},000 --> {e_h:02}:{e_m:02}:{e_s:02},000\n{_dict["text"]}\n\n'
+                f'{index+1}\n{s_h:02}:{s_m:02}:{s_s:02},{s_ms:03} --> {e_h:02}:{e_m:02}:{e_s:02},{e_ms:03}\n{_dict["text"]}\n\n'
             )
     return output_file
 
@@ -38,12 +45,12 @@ def root():
 @app.route("/whisper/transcribe", methods=["POST"])
 def whisper_transcribe():
     input_file = request.files["file"]
-    segment_length = 1
-    timelag = 0
-    output = create_textfile(filename=input_file, sr=segment_length, timelag=timelag)
+    file_path = os.path.join(SAVE_DIR, input_file.filename)  # 保存先のパス
+    input_file.save(file_path)  # 入力音声を保存
+
+    segment_length = 1  # 字幕を切り出す長さ
+    timelag = 0  # タイムコードのオフセット
+    output = create_textfile(filename=file_path, sr=segment_length, timelag=timelag)
     with open(output, "r") as file:
         contents = file.read()
         return contents
-
-
-# app.run(port=4000, debug=True)
